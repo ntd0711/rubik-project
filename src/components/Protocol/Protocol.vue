04132a4ad00e8f83d769440dcd2620ffad16f64d
hiep1999hppro <hoanghiep1999hp@gmail.com>
2022-03-29 10:48:13 +0700

    update

--- src/components/Protocol/Protocol.vue
+++ src/components/Protocol/Protocol.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="protocol" id="protocol">
-    <h2 class="protocol-label" data-aos="fade-left">Stablecoin Minting Protocol</h2>
-    <img :src="protocol" alt="roadmap-image" class="protocol-img" data-aos="fade-right" />
-    <div class="protocol-content" data-aos="fade-left">
+    <h2 class="protocol-label">Stablecoin Minting Protocol</h2>
+    <img :src="protocol" alt="roadmap-image" class="protocol-img" />
+    <div class="protocol-content">
       We've done it carefully and simply. 
       Combined with the ingredients makes for beautiful landings. 
       It is definitely the tool you need to speed up your design process.
